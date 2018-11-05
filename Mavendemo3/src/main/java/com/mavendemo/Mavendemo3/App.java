77b35df8a64af4cfb9c5ab1c2ca7cea1747a902b
JG00568405 <JG00568405@INBASDPC09646.TechMahindra.com>
2018-11-05 14:21:31 +0530

    my frist commit

---
+++ Mavendemo3/src/main/java/com/mavendemo/Mavendemo3/App.java
@@ -0,0 +1,15 @@
+package com.mavendemo.Mavendemo3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Maven Test project");
+        
+    }
+}
